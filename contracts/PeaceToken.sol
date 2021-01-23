f452f5cf2e2ca8d1e1d602a15acee47f47a9223b
SaneelDaniel <saneel.daniel07@gmail.com>
2021-01-22 19:25:43 -0800

    first commit

---
+++ contracts/PeaceToken.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.16;
+
+contract PeaceToken {
+    // string name public = "Peace Token";
+
+    uint256 public totalSupply; // The total supply of PeaceTokens
+
+    // Constructor
+
+    // Set The total number of tokens
+
+    // Read The total number of tokens
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
---
+++ test/PeaceToken.js
@@ -0,0 +1,18 @@
+var PeaceToken = artifacts.require("./PeaceToken.sol");
+
+contract("PeaceToken", function (accounts) {
+  it("sets the total supply upon deployment", function () {
+    return PeaceToken.deployed()
+      .then(function (instance) {
+        tokenInstance = instance;
+        return tokenInstance.totalSupply();
+      })
+      .then(function (totalSupply) {
+        assert.equal(
+          totalSupply.toNumber(),
+          1000000,
+          "sets the total supply to 1,000,000"
+        );
+      });
+  });
+});
