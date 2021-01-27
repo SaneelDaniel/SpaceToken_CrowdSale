79e4078ab2d085343fab0c43a5aa43630f303466
SaneelDaniel <saneel.daniel07@gmail.com>
2021-01-26 20:48:36 -0800

    token sale contract & tests wired + client-app inititalized

--- test/PeaceToken.js
+++ test/PeaceToken.js
@@ -45,51 +45,171 @@ contract("PeaceToken", function (accounts) {
       });
   });
 
-  it("should send coin correctly", () => {
-    let meta;
+  it('transfers token ownership', function() {
+    return PeaceToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      // Test `require` statement first by transferring something larger than the sender's balance
+      return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
+    }).then(assert.fail).catch(function(error) {
+      // assert(error.message.indexOf('revert') >= 0, `error message must contain revert, ${error}`);
+      return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true');
+      return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+      return tokenInstance.balanceOf(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+    });
+  });
 
-    // Get initial balances of first and second account.
-    const account_one = accounts[0];
-    const account_two = accounts[1];
-
-    let account_one_starting_balance;
-    let account_two_starting_balance;
-    let account_one_ending_balance;
-    let account_two_ending_balance;
-
-    const amount = 10;
-
+  it("handles delegated token transfers", function () {
     return PeaceToken.deployed()
-      .then((instance) => {
-        meta = instance;
-        return meta.getBalance.call(account_one);
+      .then(function (instance) {
+        tokenInstance = instance;
+        fromAccount = accounts[2];
+        toAccount = accounts[3];
+        spendingAccount = accounts[4];
+        // Transfer some tokens to fromAccount
+        return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
       })
-      .then((balance) => {
-        account_one_starting_balance = balance.toNumber();
-        return meta.getBalance.call(account_two);
+      .then(function (receipt) {
+        // Approve spendingAccount to spend 10 tokens form fromAccount
+        return tokenInstance.approve(spendingAccount, 10, {
+          from: fromAccount,
+        });
       })
-      .then((balance) => {
-        account_two_starting_balance = balance.toNumber();
-        return meta.transfer(account_two, amount, { from: account_one });
+      .then(function (receipt) {
+        // Try transferring something larger than the sender's balance
+        return tokenInstance.transferFrom(fromAccount, toAccount, 9999, {
+          from: spendingAccount,
+        });
       })
-      .then(() => meta.getBalance.call(account_one))
-      .then((balance) => {
-        account_one_ending_balance = balance.toNumber();
-        return meta.getBalance.call(account_two);
+      .then(assert.fail)
+      .catch(function (error) {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "cannot transfer value larger than balance"
+        );
+        // Try transferring something larger than the approved amount
+        return tokenInstance.transferFrom(fromAccount, toAccount, 20, {
+          from: spendingAccount,
+        });
       })
-      .then((balance) => {
-        account_two_ending_balance = balance.toNumber();
-
+      .then(assert.fail)
+      .catch(function (error) {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "cannot transfer value larger than approved amount"
+        );
+        return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {
+          from: spendingAccount,
+        });
+      })
+      .then(function (success) {
+        assert.equal(success, true);
+        return tokenInstance.transferFrom(fromAccount, toAccount, 10, {
+          from: spendingAccount,
+        });
+      })
+      .then(function (receipt) {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
         assert.equal(
-          account_one_ending_balance,
-          account_one_starting_balance - amount,
-          "Amount wasn't correctly taken from the sender"
+          receipt.logs[0].event,
+          "Transfer",
+          'should be the "Transfer" event'
         );
         assert.equal(
-          account_two_ending_balance,
-          account_two_starting_balance + amount,
-          "Amount wasn't correctly sent to the receiver"
+          receipt.logs[0].args._from,
+          fromAccount,
+          "logs the account the tokens are transferred from"
+        );
+        assert.equal(
+          receipt.logs[0].args._to,
+          toAccount,
+          "logs the account the tokens are transferred to"
+        );
+        assert.equal(
+          receipt.logs[0].args._value,
+          10,
+          "logs the transfer amount"
+        );
+        return tokenInstance.balanceOf(fromAccount);
+      })
+      .then(function (balance) {
+        assert.equal(
+          balance.toNumber(),
+          90,
+          "deducts the amount from the sending account"
+        );
+        return tokenInstance.balanceOf(toAccount);
+      })
+      .then(function (balance) {
+        assert.equal(
+          balance.toNumber(),
+          10,
+          "adds the amount from the receiving account"
+        );
+        return tokenInstance.allowance(fromAccount, spendingAccount);
+      })
+      .then(function (allowance) {
+        assert.equal(
+          allowance.toNumber(),
+          0,
+          "deducts the amount from the allowance"
         );
       });
   });
+
+  it("approves tokens for delegated transfer", function () {
+    return PeaceToken.deployed()
+      .then(function (instance) {
+        tokenInstance = instance;
+        return tokenInstance.approve.call(accounts[1], 100);
+      })
+      .then(function (success) {
+        assert.equal(success, true, "it returns true");
+        return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+      })
+      .then(function (receipt) {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "Approval",
+          'should be the "Approval" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._owner,
+          accounts[0],
+          "logs the account the tokens are authorized by"
+        );
+        assert.equal(
+          receipt.logs[0].args._spender,
+          accounts[1],
+          "logs the account the tokens are authorized to"
+        );
+        assert.equal(
+          receipt.logs[0].args._value,
+          100,
+          "logs the transfer amount"
+        );
+        return tokenInstance.allowance(accounts[0], accounts[1]);
+      })
+      .then(function (allowance) {
+        assert.equal(
+          allowance.toNumber(),
+          100,
+          "stores the allowance for delegated trasnfer"
+        );
+      });
+  });
+
 });
